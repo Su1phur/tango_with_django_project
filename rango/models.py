9a797427a7af229d1324d349d7e1d063231e0734
Su1phur <icooltse@gmail.com>
2024-01-26 10:38:48 +0000

    Ch 6 Complete, Excercise not complete

--- rango/models.py
+++ rango/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # super username asus
 # super username2 sqldb
@@ -10,6 +11,14 @@ class Category(models.Model):
     name = models.CharField(max_length = 128, unique = True)
     views = models.IntegerField(default = 0)
     likes = models.IntegerField(default = 0)
+    slug = models.SlugField(unique = True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         # return_str = str(self.name) + " " + str(self.views) + " " + str(self.likes)
